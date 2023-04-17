da02c4d698501308e17323d7c01d8b6cd52fed10
XBotz77 <102786743+XBotz77@users.noreply.github.com>
2023-04-18 07:44:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '6140591519:AAHSEm3xh8TrgJY4pz9xAoXTeaabrPD6x0I'
+const id = '6254874452'
 const address = 'https://www.google.com'
 
 const app = express();
